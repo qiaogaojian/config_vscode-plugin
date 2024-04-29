b42630f2d69100e640e37815a9386ba28db8e294
qiaogaojia <qiaogaojian@vip.qq.com>
2024-04-29 18:14:06 +0800

    C:\Users\Administrator\.vscode 2024-04-29 18:14:03

--- extensions/nomicfoundation.hardhat-solidity-0.4.0/server/.mocharc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  file: ["./test/setup.ts"],
-  require: "ts-node/register",
-  spec: "test/**/*.ts",
-  timeout: 5000,
-  exit: true,
-};
--- extensions/remixproject.ethereum-remix-0.0.12/node_modules/@remix-project/core-plugin/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { OffsetToLineColumnConverter } from './lib/offset-line-to-column-converter';
-export { CompilerMetadata } from './lib/compiler-metadata';
-export { FetchAndCompile } from './lib/compiler-fetch-and-compile';
-export { CompilerImports } from './lib/compiler-content-imports';
-export { CompilerArtefacts } from './lib/compiler-artefacts';
